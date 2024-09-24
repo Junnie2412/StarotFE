52f3bb8e0263846906b515c07ccd69cab4f1c009
Junnie2412 <dungchan321@gmail.com>
2024-09-24 14:22:54 +0700

    Update BoiBai Page (CongViec SucKhoe TinhYeu)

--- starot/src/pages/TinhYeuPage/TinhYeuPage.tsx
+++ starot/src/pages/TinhYeuPage/TinhYeuPage.tsx
@@ -1,3 +1,52 @@
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import { Link } from 'react-router-dom'
+
 export default function TinhYeuPage() {
-  return <div>Tinh Yeu</div>
+  const gridItems = Array.from({ length: 10 }, (_, index) => ({
+    id: index + 1,
+    title: `Item ${index + 1}`,
+    content: `This is the content for item ${index + 1}.`
+  }))
+
+  return (
+    <>
+      <div
+        className='bg-cover bg-no-repeat flex flex-col min-h-screen'
+        style={{
+          backgroundImage:
+            "url('https://firebasestorage.googleapis.com/v0/b/starot-aa9da.appspot.com/o/DefaultImages%2FLienHeBG.png?alt=media&token=de7de1d4-a5d0-4b54-9ebc-499de1950d15')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'top'
+        }}
+      >
+        <Header />
+        <main className='w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 md:px-20 pt-7 pb-16 flex-grow'>
+          <div className='flex justify-center mt-5'>
+            <div className='px-0 lg:px-20 md:px-0 text-center'>
+              <div className='container mx-auto px-10 lg:px-20 md:px-0'>
+                <div className='grid grid-cols-2 md:grid-cols-5'>
+                  {gridItems.map((item) => (
+                    <div
+                      key={item.id}
+                      className='p-3 mb-10 transition duration-300 ease-in-out transform hover:scale-100'
+                    >
+                      <Link to='/tram-chua-lanh/tinh-yeu/ket-qua'>
+                        <img
+                          src='https://firebasestorage.googleapis.com/v0/b/starot-aa9da.appspot.com/o/DefaultImages%2FTCLTinhYeu.png?alt=media&token=66cdec36-af9c-4284-892d-668c62ed9e67'
+                          alt='Featured Image'
+                          className='w-full object-cover transition duration-300 ease-in-out transform hover:scale-110'
+                        />
+                      </Link>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+      <Footer />
+    </>
+  )
 }
