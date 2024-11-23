3e9766008d8e954950dee568fb197058a15c22a9
Junnie2412 <dungchan321@gmail.com>
2024-11-23 10:12:55 +0700

    Update

---
+++ starot/src/types/Transaction.type.ts
@@ -0,0 +1,13 @@
+export type Transaction = {
+  id: string
+  code: string
+  time: string
+  date: string
+  service: string
+  amount: string
+  user: string
+  image: string
+  email: string
+  status: string
+  result: string
+}
